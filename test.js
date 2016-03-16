0647d9ae9514f09266e8f56239b60500b93425c4
Pier-Luc Gendreau <pier_taz@yahoo.com>
2016-03-16 18:31:37 -0400

    standard

--- test.js
+++ test.js
@@ -1,30 +1,30 @@
 import test from 'ava'
 import kmToMi from './'
 
-test('unicorns', t => {
+test('unicorns', (t) => {
   t.is(kmToMi('unicorns'), undefined)
 })
 
-test('{}', t => {
+test('{}', (t) => {
   t.is(kmToMi({}), undefined)
 })
 
-test('[]', t => {
+test('[]', (t) => {
   t.is(kmToMi([]), undefined)
 })
 
-test('"0"', t => {
+test('"0"', (t) => {
   t.is(kmToMi('0'), 0)
 })
 
-test('"1.609344"', t => {
+test('"1.609344"', (t) => {
   t.is(kmToMi('1.609344'), 1)
 })
 
-test('0', t => {
+test('0', (t) => {
   t.is(kmToMi(0), 0)
 })
 
-test('1.609344', t => {
+test('1.609344', (t) => {
   t.is(kmToMi(1.609344), 1)
 })
